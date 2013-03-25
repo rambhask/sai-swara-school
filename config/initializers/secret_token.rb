7d44c14de26f48e6e89be8ee7c6b9dc1bb4d78e9
Ram Bhaskar <rambhask@mit.edu>
2013-03-25 09:45:13 -0400

    starting over

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-SaiSwaraSchool::Application.config.secret_token = '28d0e9a2ca65786a7329edc9383c25cc34d13a846cd362be68ab7385bb4013adb43b3918a0887fb24a2c3282f5cbba6f2f7a7e736b4b379d591014d4548ee143'
--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-SaiSwaraSchool::Application.routes.draw do
-  authenticated :user do
-    root :to => 'home#index'
-  end
-  root :to => "home#index"
-  devise_for :users
-  resources :users
-end
